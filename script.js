0b57711fcc0adaff1a9a09cf222730a84bf9dcb7
lucasbrentano <lucasbrentano@gmail.com>
2024-09-06 02:33:05 -0300

    Arquivo script.js completamente funcional.

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const previousKeyType = calculator.dataset.previousKeyType
 const botoesOperacoes = document.querySelector('.botoes-operacoes')
 
 keys.addEventListener('click', e =>{
-    let arrayValores = []
     let stringValores = ""
+    let resultado
 
     if (e.target.matches('button')) {
         const key = e.target
@@ -15,82 +15,108 @@ keys.addEventListener('click', e =>{
         const keyContent = key.textContent
         const displayedNum = display.textContent
         var textoDisplay = document.getElementById("user-input")
-        if (!action) {
-            if (displayedNum === "0") {
-                display.textContent = keyContent
-            } else {
-                display.textContent = displayedNum + keyContent
-            }
+
+        function containsOperator(text) {
+            return /[+\-*/]/.test(text)
+        }
+
+        if (action === "positiveOrNegative") {
+            let valor
+            valor = -display.innerText
+            display.innerText = valor.toString()
         }
 
         if (action === 'decimal') {
-            display.textContent = displayedNum + '.'
+            if (!displayedNum.includes(".")) {
+                display.textContent = displayedNum + '.'
+            }
+        }
+
+        if (action === 'delete') {
+            display.innerText = display.innerText.substring(0,display.innerText.length-1)
+            if (display.innerText.length === 0) {
+                display.textContent = "0"
+            }
         }
 
         if (action === 'add'){
-            operacao = "+"
-            display.textContent = displayedNum + '+'
+            if (!containsOperator(preview.textContent)) {
+                preview.textContent = "";
+            }
+            if (preview.textContent.includes("=")) {
+                preview.textContent = "" + display.textContent + "+"
+                display.textContent = "0"
+            } else {
+                preview.textContent += display.textContent + "+"
+                display.textContent = "0"
+            }
         }
 
         if (action === 'subtract') {
-            operacao = "-"
-            display.textContent = displayedNum + '-'
+            if (!containsOperator(preview.textContent)) {
+                preview.textContent = ""
+            }
+            if (preview.textContent.includes("=")) {
+                preview.textContent = "" + display.textContent + "-"
+                display.textContent = "0"
+            } else {
+                preview.textContent += display.textContent + "-"
+                display.textContent = "0"
+            }
         }
 
         if (action === 'multiply') {
-            operacao = "*"
-            display.textContent = displayedNum + '*'
+            if (!containsOperator(preview.textContent)) {
+                preview.textContent = ""
+            }
+            if (preview.textContent.includes("=")) {
+                preview.textContent = "" + display.textContent + "*"
+                display.textContent = "0"
+            } else {
+                preview.textContent += display.textContent + "*"
+                display.textContent = "0"
+            }
         }
 
         if (action === 'divide') {
-            operacao = "/"
-            display.textContent = displayedNum + '/'
+            if (!containsOperator(preview.textContent)) {
+                preview.textContent = ""
+            }
+            if (preview.textContent.includes("=")) {
+                preview.textContent = "" + display.textContent + "/"
+                display.textContent = "0"
+            } else {
+                preview.textContent += display.textContent + "/"
+                display.textContent = "0"
+            }
         }
 
         if (action === 'equals') {
-            console.log("Funcionou")
-            let valor1 = 0
-            let valor2 = 0
-            let resultado = 0;
-            stringValores = display.textContent
-            switch (operacao) {
-                case "+":
-                    arrayValores = stringValores.split("+")
-                    valor1 = parseFloat(arrayValores[0])
-                    valor2 = parseFloat(arrayValores[1])
-                    resultado = valor1 + valor2
-                    preview.textContent = valor1 + " + " + valor2 + " ="
-                    display.textContent = resultado
-                    break
-                case "-":
-                    arrayValores = stringValores.split("-")
-                    valor1 = parseFloat(arrayValores[0])
-                    valor2 = parseFloat(arrayValores[1])
-                    resultado = valor1 + valor2
-                    preview.textContent = valor1 + " - " + valor2 + " ="
-                    display.textContent = resultado
-                    break
-                case "*":
-                    arrayValores = stringValores.split("*")
-                    valor1 = parseFloat(arrayValores[0])
-                    valor2 = parseFloat(arrayValores[1])
-                    resultado = valor1 * valor2
-                    preview.textContent = valor1 + " * " + valor2 + " ="
-                    display.textContent = resultado
-                    break
-                case "/":
-                    arrayValores = stringValores.split("/")
-                    valor1 = parseFloat(arrayValores[0])
-                    valor2 = parseFloat(arrayValores[1])
-                    resultado = valor1 / valor2
-                    preview.textContent = valor1 + " / " + valor2 + " ="
-                    display.textContent = resultado
-                    if (valor2 === 0) {
-                        textoDisplay.style.fontSize = "34px";
-                        display.textContent = "Divisão por ZERO"
-                    }
-                    break
+            stringValores = preview.textContent + display.textContent
+            resultado = eval(stringValores)
+            if (isNaN(resultado)||resultado === Infinity||resultado === -Infinity) {
+                textoDisplay.style.fontSize = "34px";
+                preview.textContent = "0"
+                display.textContent = "Divisão por 0!"
+            } else if (stringValores.includes("%")) {
+                preview.textContent += display.textContent + "="
+                stringValores = preview.textContent
+                let arrayValores = stringValores.split("%")
+                let valorPorcentagem = parseFloat(arrayValores[0])
+                valorPorcentagem = valorPorcentagem / 100
+                let valorNumero = parseFloat(arrayValores[1])
+                let resultadoNumero = valorPorcentagem * valorNumero
+                resultado = resultadoNumero.toString()
+                display.textContent = resultado
+            } else {
+                preview.textContent += display.textContent + "="
+                display.textContent = resultado
             }
+
+        }
+        if (action === 'percentage') {
+            preview.textContent = display.textContent + "%"
+            display.textContent = "0"
         }
 
         if (!action) {
@@ -100,7 +126,9 @@ keys.addEventListener('click', e =>{
                 display.textContent = displayedNum + keyContent
             }
         }
+
         if (action === 'clear') {
+            preview.textContent = "0"
             display.textContent = "0"
             textoDisplay.style.fontSize = "96px";
         }
